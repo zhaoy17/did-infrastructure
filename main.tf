c8e0ab4226cdcf2eb2a0d1d11f13b835979f351e
Peter Zhao <peterzhao204@gmail.com>
2021-11-03 12:43:57 -0400

    reorganize file organization

--- main.tf
+++
@@ -1,9 +0,0 @@
-resource "heroku_app" "did-api-demo" {
-  name   = "did-api-demo"
-  region = "us"
-}
-
-resource "heroku_addon" "postgres" {
-  app  = heroku_app.did-api-demo.id
-  plan = "heroku-postgresql:hobby-dev"
-}
--- output.tf
+++
@@ -1,4 +0,0 @@
-output "app_url" {
-  value       = heroku_app.did-api-demo.web_url
-  description = "Application URL"
-}
